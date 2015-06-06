fa094d050a008f39e76e187f9ef89594054e1ce7
ericdouglas <eric.douglas.mail@gmail.com>
2015-06-06 08:38:47 -0300

    CI with node and angular - api test with supertest

--- pluralsight/mean/01-continuous-integration-and-deployment-for-angular-and-node/01/test/jobs-service.spec.js
+++ pluralsight/mean/01-continuous-integration-and-deployment-for-angular-and-node/01/test/jobs-service.spec.js
@@ -1,4 +1,7 @@
-var expect = require( 'chai' ).expect;
+var express = require( 'express' );
+var app     = express();
+var expect  = require( 'chai' ).expect;
+var request = require( 'supertest' );
 
 describe( 'save jobs', function() {
 
@@ -7,7 +10,24 @@ describe( 'save jobs', function() {
   it( "should validate that description is greater than 4 characters" );
   it( "should validate that description is less than 250 characters" );
 
-  it( "should pass the job to the database save" );
+  var dataSavedJob;
+  var newJob = {
+    title : 'Programmer',
+    description : 'you will be mindlessly typing for...'
+  };
+
+  it( "should pass the job to the database save", function( done ) {
+
+    request( app )
+      .post( '/api/jobs' )
+      .send( newJob )
+      .end( function( err, res ) {
+
+        expect( dataSaved ).to.deep.equal( new Job );
+
+      });
+
+  });
   it( "should return a status of 200 to the front end if the database saved" );
   it( "should return a job with an id" );
   it( "should return an error if the database failed" );
