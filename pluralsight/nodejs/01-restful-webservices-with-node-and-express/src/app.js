b5fcf861009775a9088ce6be3460b3b1d8d34e95
ericdouglas <eric.douglas.mail@gmail.com>
2015-06-08 13:22:00 -0300

    RESTFul webservices with Node - code cleanup

--- pluralsight/nodejs/01-restful-webservices-with-node-and-express/src/app.js
+++ pluralsight/nodejs/01-restful-webservices-with-node-and-express/src/app.js
@@ -12,49 +12,7 @@ var port = process.env.PORT || 3000;
 app.use( bodyParser.urlencoded({ extended : true }));
 app.use( bodyParser.json());
 
-var bookRouter = express.Router();
-
-bookRouter.route( '/books' )
-  .post( function( req, res ) {
-  
-    var book = new Book( req.body );
-
-    // console.log( book );
-    book.save();
-    res.status( 201 ).send( book );
-  
-  })
-  .get( function( req, res ) {
-
-    var query = {};
-    if ( req.query.genre ) {
-      query.genre = req.query.genre;
-    }
-    
-    Book.find( query, function( err, books ) {
-      if ( err ) {
-        res.status( 500 ).send( err )
-      } else {
-        res.json( books );
-      }
-    });
-      
-  });
-
-bookRouter.route( '/books/:bookId' )
-  .get( function( req, res ) {
-  
-    Book.findById( req.params.bookId, function( err, book ) {
-    
-      if ( err ) {
-        res.status( 500 ).send( err );
-      } else {
-        res.json( book );
-      }
-
-    });
-
-  });
+bookRouter = require( './Routes/bookRoutes' )();
 
 app.use( '/api', bookRouter );
 
