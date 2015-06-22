ecd108be04d1f9fef463bde64309e1d7d59b2dc1
Eric Douglas <eric.douglas.mail@gmail.com>
2015-06-22 11:55:22 -0300

    JS Fundamentals for ES6 - Set.has() - object as a key

--- pluralsight/javascript/04-javascript-fundamentals-for-es6/05-built-in-objects/test/05-set.spec.js
+++ pluralsight/javascript/04-javascript-fundamentals-for-es6/05-built-in-objects/test/05-set.spec.js
@@ -10,8 +10,14 @@ describe( 'Sets', function() {
   it( 'should contain 1 item when one item is added', function() {
     var set = new Set();
     set.add( 'somevalue' );
-
     expect( set.size ).to.equal( 1 );
+  });
+
+  it( 'should allow objects as a key', function() {
+    var set = new Set();
+    var key = {};
+    set.add( key );
+    expect( set.has( key )).to.be.true;
   });
 
 });
