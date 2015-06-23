88f9ea5e56874ad1f24f83b8849796095f32cd0c
Eric Douglas <eric.douglas.mail@gmail.com>
2015-06-23 12:49:04 -0300

    JS Fundamentals for ES6 - receive resolved data

--- pluralsight/javascript/04-javascript-fundamentals-for-es6/test/06-async-development/03-promise-basics.spec.js
+++ pluralsight/javascript/04-javascript-fundamentals-for-es6/test/06-async-development/03-promise-basics.spec.js
@@ -13,4 +13,16 @@ describe( 'Promises', function() {
       });
   });
 
+  it( 'should receive the resolved data', function() {
+    var promise = new Promise( function( resolve, rejected ) {
+      resolve( 1 );
+    });
+
+    promise
+      .then( function( data ) {
+        expect( data ).to.equal( 1 );
+        done();
+      });
+  });
+
 });
