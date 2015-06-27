aca9709860298e69dc800aba2799eef6594d2081
Eric Douglas <eric.douglas.mail@gmail.com>
2015-06-27 14:59:28 -0300

    Intro to Mongoose - Schema withou _id and Schema.add()

--- pluralsight/mongodb/01-introduction-to-mongoose/standup/models/standup.server.model.js
+++ pluralsight/mongodb/01-introduction-to-mongoose/standup/models/standup.server.model.js
@@ -9,3 +9,17 @@ var standupSchema = new Schema({
   impediment : String,
   createdOn : { type : Data, default : Date.now }
 });
+
+// disabled _id
+var noIdSchema = new Schema({ name : String } , { _id : false });
+
+// Example of using Schema.add
+var exampleSchema = new Schema;
+
+exampleSchema.add({
+  project : String,
+  workYesterday : String,
+  workToday : String,
+  impediment : String,
+  createdOn : { type : Date, default : Date.now }
+});
