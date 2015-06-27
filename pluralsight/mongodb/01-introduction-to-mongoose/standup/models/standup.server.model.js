2f97bdede2a17d41bbb3780b427e654de1700c0c
Eric Douglas <eric.douglas.mail@gmail.com>
2015-06-27 15:37:06 -0300

    Intro to Mongoose - Export a model created with a schema

--- pluralsight/mongodb/01-introduction-to-mongoose/standup/models/standup.server.model.js
+++ pluralsight/mongodb/01-introduction-to-mongoose/standup/models/standup.server.model.js
@@ -1,5 +1,5 @@
 var mongoose = require( 'mongoose' );
-var Schema = mongoose.Schema;
+var Schema   = mongoose.Schema;
 
 var standupSchema = new Schema({
   memberName : String,
@@ -10,35 +10,5 @@ var standupSchema = new Schema({
   createdOn : { type : Data, default : Date.now }
 });
 
-// disabled _id
-var noIdSchema = new Schema({ name : String } , { _id : false });
-
-// Example of using Schema.add
-var includeMiddleName = true;
-
-var exampleSchema = new Schema;
-
-if ( includeMiddleName ) {
-  exampleSchema.add({
-    memberName : {
-      first : String,
-      middle : String,
-      last : String
-    }
-  });
-} else {
-  exampleSchema.add({
-    memberName : {
-      first : String,
-      last : String
-    }
-  });
-}
-
-exampleSchema.add({
-  project : String,
-  workYesterday : String,
-  workToday : String,
-  impediment : String,
-  createdOn : { type : Date, default : Date.now }
-});
+// Export model
+module.exports = mongoose.model( 'Standup', standupSchema /*, collection Name*/ );
