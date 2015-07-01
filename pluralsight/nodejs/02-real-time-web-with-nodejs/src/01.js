17222d1645ed57a6bbf667186bd5d05b82e48ac2
Eric Douglas <eric.douglas.mail@gmail.com>
2015-07-01 06:06:31 -0300

    Real-time web with Node - Adding help

--- pluralsight/nodejs/02-real-time-web-with-nodejs/src/01.js
+++ pluralsight/nodejs/02-real-time-web-with-nodejs/src/01.js
@@ -1,6 +1,19 @@
-#!/usr/bin/env node
+//#!/usr/bin/env node
+function printHelp() {
+  console.log( "01.js (c) Eric Douglas" );
+  console.log( "" );
+  console.log( "usage:" );
+  console.log( "--help          print this help" );
+  console.log( "--name          say hello to {NAME}" );
+  console.log( "" );
+}
 
 var args = require( 'minimist' )( process.argv.slice( 2 ), { string : "name" });
+
+if ( args.help || !args.name ) {
+  printHelp();
+  process.exit( 1 );
+}
 
 var name = args.name;
 
