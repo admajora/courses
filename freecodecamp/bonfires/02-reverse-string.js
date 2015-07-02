b21c0cff583be18d971a817c8cf5d17f473ecc3f
Eric Douglas <eric.douglas.mail@gmail.com>
2015-07-02 12:20:04 -0300

    FreeCodeCamp Bonfires - create reverseString tests

---
+++ freecodecamp/bonfires/02-reverse-string.js
@@ -0,0 +1,3 @@
+// Reverse String - Difficulty: 1/5
+// Reverse the provided string
+// Your result must be a string
---
+++ freecodecamp/bonfires/test/02-reverse-string.spec.js
@@ -0,0 +1,22 @@
+var expect = require( 'chai' ).expect;
+var reverseString = require( '../02-reverse-string' );
+
+describe( 'Reverse a String', function() {
+
+  it( 'Should return a string', function() {
+    expect( reverseString( 'any input' )).to.be.a( 'string' );
+  });
+
+  it( 'Should revert "hello"', function() {
+    expect( reverseString( 'hello' )).to.equal( 'olleh' );
+  });
+
+  it( 'Should revert "Howdy"', function() {
+    expect( reverseString( 'Howdy' )).to.equal( 'ydwoH' );
+  });
+
+  it( 'Should revert "Greetings from Earth"', function() {
+    expect( reverseString( 'Greetings from Earth')).to.equal( 'htraE morf sgniteerG' );
+  });
+
+});
