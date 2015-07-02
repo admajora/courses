8a115ba6fc34aa2ff66257a94019b643be432254
Eric Douglas <eric.douglas.mail@gmail.com>
2015-07-02 11:24:32 -0300

    FreeCodeCamp Bonfires - add meetBonfire test and test setup

---
+++ freecodecamp/bonfires/test/01-meet-bonfire.spec.js
@@ -0,0 +1,10 @@
+var expect = require( 'chai' ).expect;
+var meetBonfire = require( '../01-meet-bonfire' );
+
+describe( 'Meet Bonfire', function() {
+
+  it( 'Should return a boolean', function() {
+    expect( meetBonfire()).to.be.a( 'boolean' );
+  });
+
+});
