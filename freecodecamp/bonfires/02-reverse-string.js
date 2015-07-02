0942fbbea891ba70a0cfa25d55722869a476a217
Eric Douglas <eric.douglas.mail@gmail.com>
2015-07-02 12:25:24 -0300

    FreeCodeCamp Bonfires - create reverseString function

--- freecodecamp/bonfires/02-reverse-string.js
+++ freecodecamp/bonfires/02-reverse-string.js
@@ -1,3 +1,16 @@
 // Reverse String - Difficulty: 1/5
 // Reverse the provided string
 // Your result must be a string
+
+function reverseString( input ) {
+  var word = input.toString();
+
+  word = word
+    .split( '' )
+    .reverse()
+    .join( '' );
+
+  return word;
+}
+
+module.exports = reverseString;
