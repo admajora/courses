d4d724d12d852182074107068d8668212933a6ae
Eric Douglas <eric.douglas.mail@gmail.com>
2015-07-04 22:04:55 -0300

    FreeCodeCamp Bonfires - confirm end tests

---
+++ freecodecamp/bonfires/08-confirm-end.js
@@ -0,0 +1,13 @@
+/*
+ * Confirm the Ending - Difficulty: 1/5
+ *
+ * - Check if a string (first argument) ends with the given target string (second argument).
+ *
+ * Source: http://www.freecodecamp.com/challenges/bonfire-confirm-the-ending
+ */
+
+function end( str, target ) {
+
+}
+
+module.exports = end;
---
+++ freecodecamp/bonfires/test/08-confirm-end.spec.js
@@ -0,0 +1,26 @@
+var expect = require( 'chai' ).expect;
+var end    = require( '../08-confirm-end' );
+
+describe( '#end()', function() {
+
+  it( 'Should return true if target is equal the end of the string', function() {
+    expect( end( "Bastian", "n" )).to.be.true;
+  });
+
+  it( 'Should return false if target is different the end of the string', function() {
+    expect( end( "Connor", "n" )).to.be.false;
+  });
+
+  it( 'Should return false if target is different the end of the string', function() {
+    expect( end( "Walking on water and developing software from a specification are easy if both are frozen.", "specification" )).to.be.false;
+  });
+
+  it( 'Should return true if target is equal the end of the string', function() {
+    expect( end( "He has to give me a new name", "name" )).to.be.true;
+  });
+
+  it( 'Should return false if target is different the end of the string', function() {
+    expect( end( "If you want to save our world, you must hurry. We dont know how much longer we can withstand the nothing", "mountain" )).to.be.false;
+  });
+
+});
