002546a8b77d8212d24124a4215d0f633166f0ed
Eric Douglas <eric.douglas.mail@gmail.com>
2015-07-04 22:08:02 -0300

    FreeCodeCamp Bonfires - largestNumber test when passed a non array value

--- freecodecamp/bonfires/test/07-largest-number-in-array.spec.js
+++ freecodecamp/bonfires/test/07-largest-number-in-array.spec.js
@@ -11,4 +11,8 @@ describe( '#largestNumber()', function() {
     expect( largestNumber([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]])).to.deep.equal([9,35,97,1000000]);
   });
 
+  it( 'Should thrown an error if a non array is passed', function() {
+    expect( largestNumber.bind( largestNumber, 13 )).to.throw( TypeError );
+  });
+
 })
