1da732617aa89b3bf1448975cc04bf8f6f091df6
Eric Douglas <eric.douglas.mail@gmail.com>
2015-07-05 22:31:44 -0300

    FreeCodeCamp Bonfires - chunky monkey tests

---
+++ freecodecamp/bonfires/11-chunky-monkey.js
@@ -0,0 +1,15 @@
+/*
+ * Chunky Monkey - Difficulty: 1/5
+ *
+ * - Write a function that splits an array (first argument) 
+ *   into groups the length of size (second argument)
+ * - Returns them as a multidimensional array.
+ *
+ * Source: http://www.freecodecamp.com/challenges/bonfire-chunky-monkey
+ */
+
+function chunk( arr, size ) {
+
+}
+
+module.exports = chunk;
