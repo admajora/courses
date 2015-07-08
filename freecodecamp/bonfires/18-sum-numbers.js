9a7b982d5b56ea59a5c2329c27331be007a677bd
Eric Douglas <eric.douglas.mail@gmail.com>
2015-07-08 15:59:25 -0300

    FreeCodeCamp Bonfires - sum all number in a range function

--- freecodecamp/bonfires/18-sum-numbers.js
+++ freecodecamp/bonfires/18-sum-numbers.js
@@ -9,7 +9,23 @@
  */
 
 function sumAll( arr ) {
+  var arrayOfNumbers = [];
+  var max = Math.max.apply( null, arr );
+  var min = Math.min.apply( null, arr );
+  var i   = min;
 
+  while ( i <= max ) {
+    arrayOfNumbers.push( i );
+    i += 1;
+  }
+
+  var result = arrayOfNumbers.reduce( _sumTwoValues );
+
+  return result;
+}
+
+function _sumTwoValues( a, b ) {
+  return a + b;
 }
 
 module.exports = sumAll;
