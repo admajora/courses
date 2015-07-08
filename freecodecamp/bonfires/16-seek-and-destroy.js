1c94b4eab59f703fb1b6646c7bd0e1716547f463
Eric Douglas <eric.douglas.mail@gmail.com>
2015-07-08 15:06:26 -0300

    FreeCodeCamp Bonfires - seek and destroy function

--- freecodecamp/bonfires/16-seek-and-destroy.js
+++ freecodecamp/bonfires/16-seek-and-destroy.js
@@ -1,5 +1,16 @@
 module.exports = destroyer;
 
 function destroyer( arr ) {
+  var args   = [].slice.call( arguments, 0 );
+  var array  = args.shift();
+  var result = [];
 
+  array
+    .forEach( function( item ) {
+      if ( args.indexOf( item ) < 0 ) {
+        result.push( item );
+      }
+    });
+
+  return result;
 }
--- freecodecamp/bonfires/test/16-seek-and-destroy.spec.js
+++ freecodecamp/bonfires/test/16-seek-and-destroy.spec.js
@@ -4,11 +4,11 @@ var destroyer = require( '../16-seek-and-destroy' );
 describe( '#destroyer()', function() {
 
   it( 'Should return an array with "undestroyed" values', function() {
-    expect( destroyer([ 1, 2, 3, 1, 2, 3 ])).to.deep.equal([ 1, 1 ]);
+    expect( destroyer([ 1, 2, 3, 1, 2, 3 ], 2, 3 )).to.deep.equal([ 1, 1 ]);
   });
 
   it( 'Should return an array with "undestroyed" values', function() {
-    expect( destroyer([ 1, 2, 3, 5, 1, 2, 3 ])).to.deep.equal([ 1, 5, 1 ]);
+    expect( destroyer([ 1, 2, 3, 5, 1, 2, 3 ], 2, 3 )).to.deep.equal([ 1, 5, 1 ]);
   });
 
 });
