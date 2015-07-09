7d5ad2d5b88982157c942b08cbb186398fb8ba90
Eric Douglas <eric.douglas.mail@gmail.com>
2015-07-09 11:28:40 -0300

    FreeCodeCamp Bonfires - roman numeral converter tests

--- freecodecamp/bonfires/test/20-roman-numeral-converter.spec.js
+++ freecodecamp/bonfires/test/20-roman-numeral-converter.spec.js
@@ -1,2 +1,26 @@
 var expect = require( 'chai' ).expect;
 var convert = require( '../20-roman-numeral-converter' );
+
+describe( '#convert() roman numeral converter', function() {
+
+  it( 'Should convert decimal number to roman numeral', function() {
+    expect( convert( 12 )).to.equal( 'XII' );
+  });
+
+  it( 'Should convert decimal number to roman numeral', function() {
+    expect( convert( 5 )).to.equal( 'V' );
+  });
+
+  it( 'Should convert decimal number to roman numeral', function() {
+    expect( convert( 9 )).to.equal( 'IX' );
+  });
+
+  it( 'Should convert decimal number to roman numeral', function() {
+    expect( convert( 29 )).to.equal( 'XXIX' );
+  });
+
+  it( 'Should convert decimal number to roman numeral', function() {
+    expect( convert( 16 )).to.equal( 'XVI' );
+  });
+
+});
