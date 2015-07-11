2a428cd7677728a1d13614c939deb88b280420e8
Eric Douglas <eric.douglas.mail@gmail.com>
2015-07-10 21:26:05 -0300

    FreeCodeCamp Bonfires - missing letters function

--- freecodecamp/bonfires/24-missing-letters.js
+++ freecodecamp/bonfires/24-missing-letters.js
@@ -8,7 +8,29 @@
  */
 
 function fearNotLetter( str ) {
+  var result = '';
+  var charCodes = [];
 
+  str
+    .split( '' )
+    .forEach( function( letter ) {
+      charCodes.push( letter.charCodeAt() );
+    });
+
+  charCodes
+    .forEach( function( code, index, array ) {
+      var helper = array[ index - 1 ] || array[ 0 ];
+
+      if ( code - helper > 1 ) {
+        result += String.fromCharCode( helper + 1 );
+      }
+    });
+
+  if ( result === '' ) {
+    return undefined;
+  }
+
+  return result;
 }
 
 module.exports = fearNotLetter;
