f58db3d1960c969fb88cab14928c9213d132735a
Eric Douglas <eric.douglas.mail@gmail.com>
2015-07-11 17:18:18 -0300

    FreeCodeCamp Bonfires - convert html entities function

--- freecodecamp/bonfires/27-convert-html.js
+++ freecodecamp/bonfires/27-convert-html.js
@@ -7,7 +7,14 @@
  */
 
 function convert( str ) {
+  var result = str
+    .replace( /&/gi, '&amp;' )
+    .replace( /</g, '&lt;' )
+    .replace( />/g, '&gt;' )
+    .replace( /"/g, '&quot;' )
+    .replace( /'/g, '&apos;' );
 
+  return result;
 }
 
 module.exports = convert;
