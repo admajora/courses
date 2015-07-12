7a9ad1fcb964745440fa0b2d4eb7cac1f2b13fb7
Eric Douglas <eric.douglas.mail@gmail.com>
2015-07-12 16:40:33 -0300

    FreeCodeCamp Bonfires - binary agents tests

---
+++ freecodecamp/bonfires/35-binary-agents.js
@@ -0,0 +1,14 @@
+/*
+ * Binary Agents - Difficulty: 2/5
+ *
+ * - Return an English translated sentence of the passed binary string.
+ * - The binary string will be space separated.
+ *
+ * Source: http://www.freecodecamp.com/challenges/bonfire-binary-agents
+ */
+
+function binaryAgent( str ) {
+
+}
+
+module.exports = binaryAgent;
